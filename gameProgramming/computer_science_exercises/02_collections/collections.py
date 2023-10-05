cea9e0e16ff5f99b70ad26cf1959239b5e46c125
twofoe <iamshecaressa@gmail.com>
2023-10-05 13:18:50 -0400

    Update collections.py

--- gameProgramming/computer_science_exercises/02_collections/collections.py
+++ gameProgramming/computer_science_exercises/02_collections/collections.py
@@ -41,9 +41,24 @@ classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
 #print(classGPA)
 
 # pause -- WYOC -- Change 5th Element 
-breakfastFoods[4] = "Bagel"
-testScores[4] = 45
-classGPA[4] = 2.45
+# breakfastFoods[4] = "Bagel"
+# testScores[4] = 45
+# classGPA[4] = 2.45
+# print(breakfastFoods)
+# print(testScores)
+# print(classGPA)
+
+# adding and inserting items to a list 
+# .append() adds an item to the END of a list 
+breakfastFoods.append("hash browns")
 print(breakfastFoods)
+testScores.append(99)
 print(testScores)
+classGPA.append(1.99)
 print(classGPA)
+
+# .insert()allows you to place an item at a specific index in the list 
+breakfastFoods.insert(3, "Parfait")
+print(breakfastFoods)
+testScores.insert(3, 55)
+print(testScores)
