813440087934882e9dce00cadefbf4f46c232004
twofoe <iamshecaressa@gmail.com>
2023-10-05 13:10:11 -0400

    v0.2a
    Changing elements in an array

--- gameProgramming/computer_science_exercises/02_collections/collections.py
+++ gameProgramming/computer_science_exercises/02_collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examples, Zac Scott, V0.1d
+# Collections Examples, Zac Scott, V0.2a
 
 # LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES 
 breakfastFoods = ["Bacon", "Waffles", "Pancakes", "cereal", "Milk"]
@@ -30,7 +30,20 @@ classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
 #print(classGPA[2])
 
 #changing Items in a list
-breakfastFoods[0] = "Sauage"
-testScores[0] = 97
-classGPA[0] = 3.57
+#breakfastFoods[0] = "Sauage"
+#testScores[0] = 97
+#classGPA[0] = 3.57
+#print(breakfastFoods[0])
+#print(testScores[0])
+#print(classGPA[0])
+#print(breakfastFoods)
+#print(testScores)
+#print(classGPA)
 
+# pause -- WYOC -- Change 5th Element 
+breakfastFoods[4] = "Bagel"
+testScores[4] = 45
+classGPA[4] = 2.45
+print(breakfastFoods)
+print(testScores)
+print(classGPA)
