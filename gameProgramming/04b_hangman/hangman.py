a601f967d9a1f054fed236fd770dd23eab2c686f
twofoe <iamshecaressa@gmail.com>
2023-11-01 13:12:16 -0400

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,5 +1,5 @@
 import random 
-wordList = 'deal bill real steal less best test rest desk perk work dirt turn bro will teal lee bee tea chicken football sports fat lunch school middle low black blue yellow red white green brown coast jackson mandarin sandalwood bault westside flecter'.split()
+wordList = 'deal bill real steal less best test rest desk perk work dirt turn bro will teal lee bee tea chicken football sports fat lunch school middle low black blue sea'.split()
 #.split() will split a string into separate elements, by default based on SPACE
 
 # VARIABLE_NAMES that are ALL CAPS AREE CONSTANTS and not meant to change
@@ -48,9 +48,16 @@ HANGMAN_BOARD = ['''
        =====''']
 
 def getRandomWord(wordList):
-    pass
+    wordIndex = random.randint(0 , len(wordList) - 1)
+    # len(listName) - 1 is most common way to prevent index out of range errors
+    # print(wordIndex)
+    return wordList[wordIndex]
+
+def displayBoard(incorrectLetters, correctLetters, secretWord):
+    print(HANGMAN_BOARD[len(incorrectLetters)])
+    print()
 
 # i = 0
-# while i < len(HANGMAN_BOARD):
-#    print(HANGMAN_BOARD[i])
+# while i < 10:
+#    getRandomWord(wordList)
 #    i += 1 
