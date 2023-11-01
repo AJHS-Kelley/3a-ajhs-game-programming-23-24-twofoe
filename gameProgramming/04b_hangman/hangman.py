86c364bf4fa707ef765939e4b8d2d59c089ce179
twofoe <iamshecaressa@gmail.com>
2023-11-01 11:40:42 -0400

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,2 +1,2 @@
-wordList = [' word1 word2 word3 ... word30'
-]
+wordList = 'deal bill real steal less best test rest desk perk work dirt turn bro will teal lee bee tea chicken football sports fat lunch school middle low black blue yellow red white green brown coast jackson mandarin sandalwood bault westside flecter'.split()
+#.split() will split a string into separate elements, by default based on SPACE
