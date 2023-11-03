219e0b3bf8dd47a7ce0ea37dfeb211ac6b8d40f0
twofoe <iamshecaressa@gmail.com>
2023-11-03 13:13:37 -0400

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -68,6 +68,7 @@ def displayBoard(missedLetters, correctLetters, secretWord):
     for i in range(len(secretWord)):
         if secretWord[i] in correctLetters: 
             blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
             # The : character is used to SLICE strings into pieces.
             # [:i] means slice from the start until index i
             # [i+1:] means slice from i+1 until the END
@@ -77,7 +78,21 @@ def displayBoard(missedLetters, correctLetters, secretWord):
         print(letter, end = ' ')
     print()
 
-# i = 0
-# while i < 10:
-#    getRandomWord(wordList)
-#    i += 1 
+def getGuess(alreadyGuessed):
+    # Only allow: single character, A-Z only, not guessed already
+    while True: # default 'infinite' loop
+        print('Please guess a letter and press enter.')
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('Please enter a single letter.\n')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please enter an English letter only.')
+        elif guess in alreadyGuessed:
+            print('Already guessed. Please guess a different letter!')
+        else:
+            return guess 
+        
+def playAgain():
+    print('Do you want to play again. Yes or No, then press enter.')
+    return input().lower().startswith('y') # Return True/False based on input       
